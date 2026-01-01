b957af57b4ad2e6b8d2d0369040fc1e2076fa6c7
A S H <81229115+ashwinjadhav818@users.noreply.github.com>
2026-01-01 14:46:39 +0530

    st: change borderpx

--- st/config.h
+++ st/config.h
@@ -4,7 +4,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Pragmasevka:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 0;
 
 static char *shell = "/bin/sh";
 char *utmp = NULL;
