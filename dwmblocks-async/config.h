e46c4e0a35a7ccd0777ec4344ad844521427b3ec
A S H <81229115+ashwinjadhav818@users.noreply.github.com>
2026-01-05 10:28:07 +0530

    feat: new dwmblocks with icons and updated scripts

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -11,17 +11,17 @@
 #define CLICKABLE_BLOCKS 1
 
 // Control whether a leading delimiter should be prepended to the status.
-#define LEADING_DELIMITER 1
+#define LEADING_DELIMITER 0
 
 // Control whether a trailing delimiter should be appended to the status.
-#define TRAILING_DELIMITER 1
+#define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("task: ", "org-clock-task", 30, 12) \
-    X("net: ", "sb-network", 20, 5) \
-    X("vol: ", "sb-volume", 1, 4) \
-    X("bat: ", "sb-battery", 30, 3) \
-    X("date: ", "date '+%a %b %d %I:%M %p'", 30, 1) \
+    X("  ", "org-clock-task", 30, 12) \
+    X("", "sb-network", 20, 5) \
+    X("", "sb-volume", 1, 4) \
+    X("", "sb-battery", 30, 3) \
+    X("󰃭 ", "date '+%a %b %d %I:%M %p'", 30, 1) \
 
 #endif  // CONFIG_H
